909f6c8096ad2212614a61c47b18c6b744340533
mustafaquraish <mustafaq9@gmail.com>
2024-11-01 19:27:14 +0000

    Deploying to gh-pages from @ ocen-lang/og-wasm-template@64c1c38b115574b6707320a4489e240d130b34d6 🚀

--- mod2.js
+++ mod2.js
@@ -58,7 +58,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpihevnbrv.js
+// include: /tmp/tmpc31w0oko.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -249,21 +249,21 @@ Module['FS_createPath']("/", "assets", true, true);
 
   })();
 
-// end include: /tmp/tmpihevnbrv.js
-// include: /tmp/tmpo9wvz4xz.js
+// end include: /tmp/tmpc31w0oko.js
+// include: /tmp/tmp0qs39qjp.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpo9wvz4xz.js
-// include: /tmp/tmp3gpzu5hr.js
+  // end include: /tmp/tmp0qs39qjp.js
+// include: /tmp/tmpqbmm_xoc.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp3gpzu5hr.js
+  // end include: /tmp/tmpqbmm_xoc.js
 
 
 // Sometimes an existing Module object exists with properties
